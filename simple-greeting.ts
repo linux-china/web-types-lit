aeeefd83067adbc97e26f681d7206d49ed2950bf
linux_china <libing.chen@gmail.com>
2021-07-04 14:25:51 -0700

    simple-greeting.ts

---
+++ simple-greeting.ts
@@ -0,0 +1,22 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+    static styles = css`p {
+      color: blue
+    }`;
+
+    @property()
+    name = 'Somebody';
+
+    render() {
+        return html`<p>Hello, ${this.name}!</p>
+        <button @click=${this.clickButton}>click</button>`;
+    }
+
+    clickButton() {
+        this.name = 'Clicked';
+    }
+
+}
